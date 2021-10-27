6a50735ee8b122608480eb3e24989221ffde5969
CatanaH <catanaharrington@Hotmail.com>
2021-10-27 12:41:44 -0700

    getting basic app running

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,16 @@
+// import 'react-native-gesture-handler';
+import React, {useEffect} from 'react';
+// import { Button, View, Text, ScrollView, StatusBar, SafeAreaView} from 'react-native';
+import { Text, View } from 'react-native';
+// import styles from '../styles/styles'
+
+const HomeScreen = () => {
+
+    return(
+        <View >
+            <Text >Find your next shore diving adventure</Text>
+        </View>
+    )
+}
+
+export default HomeScreen;
